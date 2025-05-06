b44e20fba51481b900aa1158aa5859d8ed495009
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-05-06 13:58:15 +0500

    chagnge db connection uri

--- startup/db.js
+++ startup/db.js
@@ -2,11 +2,10 @@ import { connect } from "mongoose";
 import winston from "winston";
 
 export const db = async () => {
-    try {
-        // // mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.cbm1vlz.mongodb.net/virtualdars
-        await connect(process.env.MONGO_URI);
-        winston.debug("MongoDB ga ulanish hosil qilindi!");
-    } catch (err) {
-        winston.error("Ulanishda xatolik yuz berdi ", err);
-    }
+  try {
+    await connect(process.env.MONGO_URI);
+    winston.debug("MongoDB ga ulanish hosil qilindi!");
+  } catch (err) {
+    winston.error("Ulanishda xatolik yuz berdi ", err);
+  }
 };
